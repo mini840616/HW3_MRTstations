e2cff776385c169108b855a9aaf8809ca44498ca
mini840616 <66chiang@gmail.com>
2016-12-27 13:54:39 +0800

    committed

---
+++ Package.swift
@@ -0,0 +1,11 @@
+import PackageDescription
+
+let package = Package(
+    name: "MRTStation",
+    targets: [],
+    dependencies: [
+        .Package(url: "https://github.com/SwiftyJSON/SwiftyJSON.git", versions: Version(1,0,0)..<Version(3, .max, .max)),
+    ]
+)
+
+
